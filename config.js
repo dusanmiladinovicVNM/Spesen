45d05ff9db4adcf7608bea3463b6bff35b5c8586
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-08-03 13:29:24 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-/* Jedina datoteka koju mijenjaš pri postavljanju.
-   URL je "Web app URL" iz Apps Script → Deploy → Manage deployments. */
-const CONFIG = {
-  url: 'https://script.google.com/macros/s/AKfycbwERKKiXRwXqzgSSX7_XcFp3QImEpw5jc9ZfEqzrCXlHfJ2FOWAE52oZaWieQWF-p6Spw/exec'
-};
