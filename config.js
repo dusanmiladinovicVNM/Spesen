effac844ac0f8627c3a05768d6ca6d5870b30243
dusanmiladinovicVNM <162486613+dusanmiladinovicVNM@users.noreply.github.com>
2026-08-03 12:39:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Jedina datoteka koju mijenjaš pri postavljanju.
    URL je "Web app URL" iz Apps Script → Deploy → Manage deployments. */
 const CONFIG = {
-  url: 'https://script.google.com/macros/s/OVDJE_ID_DEPLOYMENTA/exec'
+  url: 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwERKKiXRwXqzgSSX7_XcFp3QImEpw5jc9ZfEqzrCXlHfJ2FOWAE52oZaWieQWF-p6Spw/exec/exec'
 };
